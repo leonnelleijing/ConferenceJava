20c0422ee4e360392d575c6d2af5a91b35eb0455
leonnelleijing <leonleijing@gmail.com>
2021-02-03 12:21:11 +0100

    update of test file

--- src/test/java/tp10/metier/JourTest.java
+++ src/test/java/tp10/metier/JourTest.java
@@ -16,9 +16,28 @@ public class JourTest {
     
     public JourTest() {
     }
-
+    public static final int nbJour =2;
     @org.junit.jupiter.api.Test
-    public void testSomeMethod() {
+    public void testConstructorJour() {
+        Jour j =new Jour(nbJour);
+        assertSame(nbJour,j.getNoJour());
     }
+    
+    @org.junit.jupiter.api.Test
+    public void testToStringJour() {
+        Jour j =new Jour(nbJour);
+        assertEquals("Jour " +nbJour,j.toString(),"to String doesn't works");
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testHashEqual(){
+        Jour j1=new Jour(nbJour);
+        Jour j2=new Jour(nbJour);
+        Jour j3=new Jour(3);
+        assertEquals(j1.hashCode(),j2.hashCode(),"we are the same");
+        assertEquals(j1,j2,"we are the same");
+        assertNotEquals(j3,j2,"we are not the same");
+    }
+    
     
 }
